76c7aea1d6031c084aa571f847fc73596c54498c
Sathish <sathishsekar.ss4@gmail.com>
2024-10-26 22:39:54 +0530

    Initial qr scanner changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ZXingScannerModule} from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ZXingScannerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'qrScanner';
+  qrData:string = 'no Data found';
+  getQRData(event:any){
+    if(event){
+      this.qrData=event as string;
+    }
+  }
 }
