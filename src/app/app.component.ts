4e3c47986e7d838e391295333d70a4abd2a8b440
Sathish <sathishsekar.ss4@gmail.com>
2024-10-27 15:34:44 +0530

    method documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,10 @@ import {ZXingScannerModule} from '@zxing/ngx-scanner';
 })
 export class AppComponent {
   qrData:string = 'no Data found';
+  /**
+   * Method to handle the emitted qr data from the scanner
+   * @param event - event of type any ( this contains qr data )
+   */
   getQRData(event:any){
     if(event){
       this.qrData=event as string;
